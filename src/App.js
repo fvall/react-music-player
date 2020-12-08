283a410d1e2d30f223a5e0a83b66fc38684c7b79
Felipe Valladao <felipe.valladao@gmail.com>
2020-12-08 23:31:36 +0000

    start implementing library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles/app.scss';
 
 import Player from './components/Player';
 import Song from './components/Song';
-
+import Library from './components/Library';
+import data from './util';
 
 function App() {
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song/>
-      <Player/>
+      <Song currentSong={currentSong}/>
+      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
+      <Library songs={songs}/>
     </div>
   );
 }
---
+++ src/util.js
@@ -0,0 +1,53 @@
+import {v4 as uuidv4} from 'uuid';
+
+function chillHop() {
+    return [
+        {
+            name   : "Lilo",
+            artist : "The Field Tapes",
+            cover  : "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+            id     : uuidv4(),
+            active : true,
+            audio  : "https://mp3.chillhop.com/serve.php/?mp3=11244",
+            color  : ["#82d9b8", "#a34040"]
+        },
+        {
+            name   : "Harbor",
+            artist : "The Field Tapes",
+            cover  : "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+            id     : uuidv4(),
+            active : false,
+            audio  : "https://mp3.chillhop.com/serve.php/?mp3=11245",
+            color  : ["#82d9b8", "#a34040"]
+        },
+        {
+            name   : "Going Back",
+            artist : "Swørn",
+            cover  : "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
+            id     : uuidv4(),
+            active : false,
+            audio  : "https://mp3.chillhop.com/serve.php/?mp3=10310",
+            color  : ["#82d9b8", "#a34040"]
+        },
+        {
+            name   : "Magenta Forever",
+            artist : "Aviino",
+            cover  : "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
+            id     : uuidv4(),
+            active : false,
+            audio  : "https://mp3.chillhop.com/serve.php/?mp3=10458",
+            color  : ["#82d9b8", "#a34040"]
+        },
+        {
+            name   : "Bluetooth Ringtone",
+            artist : "Aviino",
+            cover  : "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
+            id     : uuidv4(),
+            active : false,
+            audio  : "https://mp3.chillhop.com/serve.php/?mp3=10446",
+            color  : ["#82d9b8", "#a34040"]
+        },
+    ];
+}
+
+export default chillHop;
